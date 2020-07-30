dc22744449eec51459c2b1f813aa9e31612b9a81
yourgentlesmile <iamwinner01@126.com>
2020-07-30 22:30:54 +0800

    声明与使用变量

--- src/xc/StudyGo/Main.go
+++ src/xc/StudyGo/Main.go
@@ -12,9 +12,53 @@ var (
 	isOk bool
 )
 
-func main() {
+func foo() (int, string) {
+	return 10, " "
+}
+
+func varDeclare() {
 	name = "new World"
 	age = 18
 	isOk = true
-	fmt.Print("Hello world")
+	//非全局变量声明后必须使用
+	var hhh string = "123"
+	//类型推断
+	var bbb = "123"
+	//简短变量声明，只能在函数里面用
+	ccc := "456"
+	fmt.Print("Hello world" + hhh + bbb + ccc)
+
+	//匿名变量，在使用多重赋值时，如果想要忽略某个值
+	//可以使用匿名变量。匿名变量用一个下划线表示
+	//匿名变量不占用命名空间，不会分配内存，所以匿名变量之间不存在重复声明
+	x, _ := foo()
+	_, y := foo()
+	fmt.Print(x)
+	fmt.Print(y)
+
+	//定义常量的时，可以单独声明常量
+	const cc1 = "const"
+	//也可以同时声明多个常量，如果省略了值则表示和上面一行的值相同
+	const (
+		c1 = "name"
+		c2
+		c3
+		//c3 == c2 == c1
+	)
+
+	//iota是go语言的常量计数器，只能在常量的表达式中使用
+	//iota在const关键字出现时将被重置为0，const中每新增一行常量声明
+	//将使iota计数一次(iota可理解为const语句块总的行索引)
+	//也能使用 _ 跳过某些值
+	const (
+		counter1 = iota
+		counter2 // = 1
+		counter3 // = 2
+		_
+		counter4 // = 4
+	)
+}
+
+func main() {
+
 }
